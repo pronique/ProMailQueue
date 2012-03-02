712af6c86ae45f9f613a523a4486135eb8303bef
Jonathan Cutrer <joncutrer@gmail.com>
2012-03-02 17:01:23 -0600

    added __developer_hack__

--- Controller/ProMailQueueAppController.php
+++ Controller/ProMailQueueAppController.php
@@ -1,6 +1,25 @@
 <?php
-
+/**
+* ProMailQueueAppController class
+* 
+*/
 class ProMailQueueAppController extends AppController {
-
+    
+    function beforeFilter() {
+        
+        // begin __developer_hack__
+        if ( $layout = Configure::read('ProMailQueue.__developer_hack__.View.layout' ) ) {    
+            $this->layout = $layout;       
+        }    
+        // end __developer_hack__
+        
+        // begin __developer_hack__
+        if ( $scaffold = Configure::read('ProMailQueue.__developer_hack__.Controller.scaffold' ) ) {       
+            if ( $scaffold !== false ) {
+                $this->scaffold = $scaffold;
+            }       
+        }    
+        // end __developer_hack__
+    }
 }
 
